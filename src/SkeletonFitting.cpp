bc13d1a2ee8a168e4fc3c59efcc060ee5ec33e37
Garoe Dorta <garoedp@gmail.com>
2013-11-05 16:08:39 +0000

    Added empty class Skeleton Fitting

---
+++ src/SkeletonFitting.cpp
@@ -0,0 +1,18 @@
+/*
+ * SkeletonFitting.cpp
+ *
+ *  Created on: 5 Nov 2013
+ *      Author: m04701
+ */
+
+#include "SkeletonFitting.h"
+
+SkeletonFitting::SkeletonFitting() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SkeletonFitting::~SkeletonFitting() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/SkeletonFitting.h
@@ -0,0 +1,17 @@
+/*
+ * SkeletonFitting.h
+ *
+ *  Created on: 5 Nov 2013
+ *      Author: m04701
+ */
+
+#ifndef SKELETONFITTING_H_
+#define SKELETONFITTING_H_
+
+class SkeletonFitting {
+	public:
+		SkeletonFitting();
+		virtual ~SkeletonFitting();
+};
+
+#endif /* SKELETONFITTING_H_ */
