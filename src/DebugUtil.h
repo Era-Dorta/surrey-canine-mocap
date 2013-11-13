1b9d5ccf843a6abbb2a56a19263548c857157473
Garoe Dorta <garoedp@gmail.com>
2013-11-13 09:36:39 +0000

    Moved debug function to new debugutil file.

---
+++ src/DebugUtil.h
@@ -0,0 +1,17 @@
+/*
+ * DebugUtil.hpp
+ *
+ *  Created on: 12 Nov 2013
+ *      Author: m04701
+ */
+
+#ifndef DEBUGUTIL_HPP_
+#define DEBUGUTIL_HPP_
+
+//TODO Clean up all the miscutil includes that were becouse of this method
+inline std::ostream& operator<<(std::ostream &out, const osg::Vec3 &vector) {
+	out << "[" << vector.x() << ", " << vector.y() << ", " << vector.z() << "]";
+	return out;
+}
+
+#endif /* DEBUGUTIL_HPP_ */
--- src/MiscUtils.h
+++ src/MiscUtils.h
@@ -647,9 +647,4 @@ inline bool get_neighbor_white_pixel(cv::Mat& img, int i_row, int i_col,
 	return false;
 }
 
-inline std::ostream& operator<<(std::ostream &out, const osg::Vec3 &vector) {
-	out << "[" << vector.x() << ", " << vector.y() << ", " << vector.z() << "]";
-	return out;
-}
-
 #endif /* MISCUTILS_H_ */
