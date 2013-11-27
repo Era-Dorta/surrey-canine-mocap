dd7c9925c5328cfb0ea309d30e0945158d57c4d2
Garoe Dorta <garoedp@gmail.com>
2013-11-27 12:45:21 +0000

    Fixed minor error on import module. Added several comments to python script

--- blender/script.py
+++ blender/script.py
@@ -1,23 +1,27 @@
 import bpy
-import io_anim_bvh
+import io_anim_bvh.import_bvh
 from bpy_extras.io_utils import (axis_conversion)
 
 new_file_name = '/home/cvssp/misc/m04701/workspace/data/bvh/out2.bvh'
 global_scale=0.3
 global_matrix = axis_conversion('-Z',
-                               '-Y',
+                               'Y',
                                 ).to_4x4()
                                          
-import_bvh = io_anim_bvh.import_bvh.load(None, bpy.context, new_file_name,
+io_anim_bvh.import_bvh.load(None, bpy.context, new_file_name,
 	'ARMATURE', 'NATIVE', global_scale, False, 1, global_matrix )
 
 current_skel = bpy.context.scene.objects["Armature"]
-#current_skel_arm = bpy.data.armatures["Armature"]
 new_skel = bpy.context.scene.objects["out2"]
+
+#Do not show the new skeleton
+new_skel.hide = True
 
 #For each bone in the rigged skeleton add a copy transformation
 #for the new skeleton
 for bone in list(current_skel.pose.bones):
+	#If we do not want translations then uncomment this line and comment the next
+	#bone.constraints.new(type='COPY_ROTATION')
 	bone.constraints.new(type='COPY_TRANSFORMS')
 	constraint = bone.constraints[0]
 	constraint.target = new_skel
