8a3b2cdb140607e6811c1ce1a2093ce45d61addd
Garoe Dorta <garoedp@gmail.com>
2013-11-27 16:03:31 +0000

    Improved script to be able to update the scene if executed again

--- blender/script.py
+++ blender/script.py
@@ -2,17 +2,30 @@ import bpy
 import io_anim_bvh.import_bvh
 from bpy_extras.io_utils import (axis_conversion)
 
-new_file_name = '/home/cvssp/misc/m04701/workspace/data/bvh/out2.bvh'
+file_name = 'out2'
+file_path = '/home/cvssp/misc/m04701/workspace/data/bvh/'
+file_end = '.bvh'
+
+scene = bpy.context.scene
+
+#If the new_skeleton is already in the scene then selected it and delete it 
+if file_name in scene.objects:
+	scene.objects[file_name].select = True
+	bpy.ops.object.delete(None)
+
+full_file_path = file_path + file_name + file_end
 global_scale=0.3
 global_matrix = axis_conversion('-Z',
                                'Y',
                                 ).to_4x4()
                                          
-io_anim_bvh.import_bvh.load(None, bpy.context, new_file_name,
+io_anim_bvh.import_bvh.load(None, bpy.context, full_file_path,
 	'ARMATURE', 'NATIVE', global_scale, False, 1, global_matrix )
 
-current_skel = bpy.context.scene.objects["Armature"]
-new_skel = bpy.context.scene.objects["out2"]
+
+current_skel = scene.objects["Armature"]
+
+new_skel = scene.objects[file_name]
 
 #Do not show the new skeleton
 new_skel.hide = True
