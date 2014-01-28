93fdb61a03f68f5c07ebf5b7b17a523ce63f4092
Garoe Dorta <garoedp@gmail.com>
2014-01-28 15:55:10 +0000

    IMPORTANT This code modifies again the RGBDCamera to use less frames for faster development.

--- src/RGBDCamera.cpp
+++ src/RGBDCamera.cpp
@@ -51,7 +51,7 @@ RGBD_Camera::RGBD_Camera(std::string dataset_path, std::string cam_name) :
 	cout << "Found " << rgb_files.size() << " images." << endl;
 
 	//Parse the RGB filenames:
-	for (std::vector<std::string>::iterator i(rgb_files.begin() + 30);
+	for (std::vector<std::string>::iterator i(rgb_files.begin() + 80);
 			i != rgb_files.end() - 57; ++i)
 			//for(std::vector<std::string>::iterator i(rgb_files.begin()); i != rgb_files.begin()+10; ++i)
 			{
@@ -110,7 +110,7 @@ void RGBD_Camera::load_timestamps(std::map<int, double>& ts, std::string fn) {
 void RGBD_Camera::load_frame(int frame_num) {
 	RGBD_Frame frame;
 
-	frame.frame_num = frame_num - 30;
+	frame.frame_num = frame_num - 80;
 
 	//Read images:
 	char fn[1024];
@@ -130,7 +130,7 @@ void RGBD_Camera::load_frame(int frame_num) {
 		exit(-1);
 	}
 
-	frames[frame_num - 30] = frame;
+	frames[frame_num - 80] = frame;
 
 }
 
